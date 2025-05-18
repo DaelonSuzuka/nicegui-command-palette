ec121ed065ef56bc8f79d8ab03e72e1abd8687a0
David Kincaid <daelonsuzuka@gmail.com>
2025-05-17 23:34:36 -0400

    Fix items not rendering

--- src/nicegui_command_palette/command_table.vue
+++ src/nicegui_command_palette/command_table.vue
@@ -2,7 +2,7 @@
     <q-table hide-header flat class="w-full" @row-click="(e, row) => { $emit('row_clicked', row) }">
         <template v-slot:body-cell-value="props">
             <q-td :props="props">
-                {{ props.row.display }}
+                {{ props.row.label }}
             </q-td>
         </template>
         <template v-slot:no-data="">
